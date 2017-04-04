23e906e4f44cd2cf0264c49ef3eb2bc5d86e1773
WorkplaceX <info@workplacex.com>
2017-04-04 18:45:36 +0000

    Build script update

--- Framework/Build/BuildTeamServices.ps1
+++ Framework/Build/BuildTeamServices.ps1
@@ -1,21 +1,27 @@
-Write-Output $A 
-
+# Init
 $FolderName = (get-item $PSCommandPath).Directory.Parent.Parent.Parent.FullName + "\"
 $FolderNameDotNetZip = (get-item $PSCommandPath).Directory.Parent.Parent.Parent.Parent.FullName + "\"
-cd $FolderNameDotNetZip
 
 # Download .NET Core 1.1 compiler
+cd $FolderNameDotNetZip
 Invoke-WebRequest https://go.microsoft.com/fwlink/?linkid=843454 -OutFile dotnet.zip
 
 # Extract zip
 Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::ExtractToDirectory("$($FolderNameDotNetZip)dotnet.zip", "$($FolderNameDotNetZip)dotnet\")
 
+#Build
 cd $FolderName
 cd Build
 cmd.exe /c $FolderNameDotNetZip\dotnet\dotnet.exe restore
 cmd.exe /c $FolderNameDotNetZip\dotnet\dotnet.exe build
 
+# ConnectionString
+cd $FolderName
+cd Build
+cmd.exe /c $FolderNameDotNetZip\dotnet\dotnet.exe run 01 $ConnectionString
+
+# Server
 cd $FolderName
 cd Server
 cmd.exe /c $FolderNameDotNetZip\dotnet\dotnet.exe restore
