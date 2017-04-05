4fd76c107699b18d3a459c5345d25499a256a9b0
WorkplaceX <info@workplacex.com>
2017-04-05 21:03:00 +0000

    %m%

---
+++ Framework/Build/Shell/Commit.ps1
@@ -0,0 +1,14 @@
+cd ..\..\..
+git status --short
+cd ..
+git status --short
+set /p m=Commit message: 
+cd Submodule
+git add .
+git commit -m "%m%"
+git push
+cd ..
+git add .
+git commit -m "%m%"
+git push
+set /p m=
