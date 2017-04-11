a09c27925d28a19f534b9489d2b9e13245b2f0ec
WorkplaceX <info@workplacex.com>
2017-04-11 17:45:33 +0200

    Build update

--- Framework/Build/Shell/travis-ci.org/Build.sh
+++ Framework/Build/Shell/travis-ci.org/Build.sh
@@ -1,6 +1,25 @@
 PATH="/home/travis/.dotnet":$PATH
+
+FolderName=$(pwd)
+
+# Build
+cd $FolderName
 cd Build
 dotnet restore
 dotnet build
-dotnet run 01 "$ConnectionString"
-dotnet run 02
+# Set ConnectionString
+dotnet run 01 "$ConnectionString" 
+# InstallAll
+dotnet run 02 
+
+# Build RunSql
+cd $FolderName
+cd Build
+dotnet run 11
+
+# Publish Server
+cd $FolderName
+cd Server
+dotnet restore
+dotnet build
+dotnet publish
