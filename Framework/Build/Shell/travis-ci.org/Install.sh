3132999a979b9a05aecd7e99fd505b0087d7defd
WorkplaceX <info@workplacex.com>
2017-04-11 17:03:24 +0200

    Build update

--- Framework/Build/Shell/travis-ci.org/Install.sh
+++ Framework/Build/Shell/travis-ci.org/Install.sh
@@ -2,4 +2,20 @@ echo CS2=$ConnectionString
 sudo apt-get install libunwind8
 ./Submodule/Framework/Build/Shell/travis-ci.org/dotnet-install.sh
 dotnet --version
+
+# npm, node version check
+echo npm version:
+npm -version
+echo node version:
+node -version
+
+#npm update
+npm install npm@latest -g
+
+# npm, node version check
+echo npm version:
+npm -version
+echo node version:
+node -version
+
 npm install gulp
