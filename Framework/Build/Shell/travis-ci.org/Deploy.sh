c9e28733da00ca4faf748ec175d5744915a2bd2f
WorkplaceX <info@workplacex.com>
2017-04-12 11:29:25 +0200

    Build update

--- Framework/Build/Shell/travis-ci.org/Deploy.sh
+++ Framework/Build/Shell/travis-ci.org/Deploy.sh
@@ -1,7 +1,7 @@
 cd ./Server/bin/Debug/netcoreapp1.1/publish
 
 git init
-git remote add azure "$AzureGitUrl"
+git remote add azure https://AzureDeploy22:Ndc73Ocd0DwQKl@framework22.scm.azurewebsites.net:443/framework22.git
 git fetch --all
 git add .
 git commit -m Deploy
