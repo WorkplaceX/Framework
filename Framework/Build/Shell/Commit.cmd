42148d441442347a7862ee39acb9941af6ce5cb8
WorkplaceX <info@workplacex.com>
2017-04-13 21:48:38 +0200

    Commit script update

--- Framework/Build/Shell/Commit.cmd
+++ Framework/Build/Shell/Commit.cmd
@@ -12,12 +12,12 @@ git checkout master & REM Prevent detached branch
 git checkout -B "%branchName%"
 git add .
 git commit -m "%m%"
-git push
+git push -u origin "%branchName%"
 cd ..
 git checkout -B "%branchName%"
 git add .
 git commit -m "%m%"
-git push
+git push -u origin "%branchName%"
 echo ### Git Status ###
 git status --short
 cd Submodule
