2333ab6b6a79fe395dacdbe03ab671927918dc9d
WorkplaceX <info@workplacex.com>
2017-04-13 21:54:31 +0200

    Test

--- Framework/Build/Shell/travis-ci.org/Build.sh
+++ Framework/Build/Shell/travis-ci.org/Build.sh
@@ -7,28 +7,4 @@ cd $FolderName
 cd Build
 dotnet restore
 dotnet build
-# Set ConnectionString
-set +x
-dotnet run 01 "$ConnectionString" 
-set -x
-# InstallAll
-dotnet run 02 
-
-# Build RunSql
-echo \#\#\# RunSql
-cd $FolderName
-cd Build
-dotnet run 11
-
-# Publish Server
-echo \#\#\# Publish Server
-cd $FolderName
-cd Server
-dotnet restore
-dotnet build
-dotnet publish
-
-# Deploy
-cd $FolderName
-echo \#\#\# Deploy
-./Submodule/Framework/Build/Shell/travis-ci.org/Deploy.sh
+dotnet run 06
