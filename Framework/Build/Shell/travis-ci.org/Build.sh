c88e910b2de08f37faa57106998d7fcda7754e92
WorkplaceX <info@workplacex.com>
2017-04-14 14:41:04 +0200

    Script update

--- Framework/Build/Shell/travis-ci.org/Build.sh
+++ Framework/Build/Shell/travis-ci.org/Build.sh
@@ -1,14 +1,19 @@
 set -x
 
-FolderName=$(pwd)
+function Build()
+{
+	FolderName=$(pwd)
 
-echo \#\#\# Build
-cd $FolderName
-cd Build
-dotnet restore
-dotnet build
-dotnet run 06 2> Error.txt
+	echo \#\#\# Build
+	cd $FolderName
+	cd Build
+	dotnet restore
+	dotnet build
+	dotnet run 06
+}
+
+Build 2> Error.txt
+exit $?
 echo "Error2 Start"
 echo "$(<Error.txt)"
 echo "Error2 End"
-
