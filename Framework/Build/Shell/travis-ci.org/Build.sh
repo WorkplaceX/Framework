6b0b5267319784e3d792fcedf72ac7e69cef8978
WorkplaceX <info@workplacex.com>
2017-04-14 14:14:39 +0200

    Read back Error.txt

--- Framework/Build/Shell/travis-ci.org/Build.sh
+++ Framework/Build/Shell/travis-ci.org/Build.sh
@@ -9,16 +9,26 @@ dotnet restore
 dotnet build
 # Set ConnectionString
 set +x
-dotnet run 01 "$ConnectionString" 
+dotnet run 01 "$ConnectionString" 2> Error.txt
+echo "Error2 Start"
+echo "$(<Error.txt)"
+echo "Error2 End"
 set -x
 # InstallAll
-dotnet run 02 
+dotnet run 02 2> Error.txt
+echo "Error2 Start"
+echo "$(<Error.txt)"
+echo "Error2 End"
+
 
 # Build RunSql
 echo \#\#\# RunSql
 cd $FolderName
 cd Build
-dotnet run 11
+dotnet run 11 2> Error.txt
+echo "Error2 Start"
+echo "$(<Error.txt)"
+echo "Error2 End"
 
 # Publish Server
 echo \#\#\# Publish Server
