6d016c1a8bb86b36e0b27005012048a875c3d4e9
WorkplaceX <info@workplacex.com>
2017-04-16 17:50:06 +0200

    Build update

--- Framework/Build/Shell/travis-ci.org/Install.sh
+++ Framework/Build/Shell/travis-ci.org/Install.sh
@@ -1,14 +1,14 @@
 BASH_XTRACEFD=1 # Execute command to stdout. Not stderr.
 set -x # Enable print execute cammands to stdout.
 
+# https://www.microsoft.com/net/core#linuxubuntu
+sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+sudo apt-get update
+sudo apt-get install dotnet-dev-1.0.1
+
 function Install()
 {
-    # https://www.microsoft.com/net/core#linuxubuntu
-    sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-    sudo apt-get update
-    sudo apt-get install dotnet-dev-1.0.1
-
     dotnet --version
 
     # npm, node version check
