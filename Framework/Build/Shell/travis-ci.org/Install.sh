56e63b191f85d87bca79f5e4e2e99656108bccfd
WorkplaceX <info@workplacex.com>
2017-04-16 13:31:10 +0200

    Build script with stderr

--- Framework/Build/Shell/travis-ci.org/Install.sh
+++ Framework/Build/Shell/travis-ci.org/Install.sh
@@ -1,22 +1,33 @@
-set -x
+set -x # Print execute cammands
 
-# https://www.microsoft.com/net/core#linuxubuntu
-sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-sudo apt-get update
-sudo apt-get install dotnet-dev-1.0.1
+function Install()
+{
+    # https://www.microsoft.com/net/core#linuxubuntu
+    sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+    sudo apt-get update
+    sudo apt-get install dotnet-dev-1.0.1
 
-dotnet --version
+    dotnet --version
 
-# npm, node version check
-npm --version
-node --version
+    # npm, node version check
+    npm --version
+    node --version
 
-#npm update
-npm install npm@latest -g
+    #npm update
+    npm install npm@latest -g
 
-# npm, node version check
-npm --version
-node --version
+    # npm, node version check
+    npm --version
+    node --version
 
-npm install gulp
+    npm install gulp
+}
+
+Install 2> Error.txt # stderr to Error.txt
+if [ -s Error.txt ] # If Error.txt not empty
+then
+	echo "### Error"
+	echo "$(<Error.txt)"
+	exit 1 # Exit code
+fi
