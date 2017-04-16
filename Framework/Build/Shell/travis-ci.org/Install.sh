45b09e84f6e03c6eccb9123da7699d24b1f3d432
WorkplaceX <info@workplacex.com>
2017-04-16 14:27:43 +0200

    Build update

--- Framework/Build/Shell/travis-ci.org/Install.sh
+++ Framework/Build/Shell/travis-ci.org/Install.sh
@@ -1,14 +1,17 @@
+BASH_XTRACEFD=1
 set -x # Print execute cammands
+echo "Hello1"
 
 function Install()
 {
-	echo "Hello"
+	echo "Hello2"
+	# >&2 echo "Error2"
 }
 
 Install 2> Error.txt # stderr to Error.txt
 if [ -s Error.txt ] # If Error.txt not empty
 then
-	echo "### Error2"
+	echo "### Error3"
 	echo "$(<Error.txt)"
 	exit 1 # Exit code
 fi
