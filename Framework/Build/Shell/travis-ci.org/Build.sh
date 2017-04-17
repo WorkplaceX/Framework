47db62438589df6452e07c04872c03597a8ec16b
WorkplaceX <info@workplacex.com>
2017-04-17 14:17:20 +0200

    Build update

--- Framework/Build/Shell/travis-ci.org/Build.sh
+++ Framework/Build/Shell/travis-ci.org/Build.sh
@@ -54,7 +54,7 @@ function Deploy
 }
 
 cd $FolderName
-Main 2> >(tee Error.txt) # stderr to stdout and Error.txt.
+Main 2> Error.txt # Main 2> >(tee Error.txt) # stderr to stdout and Error.txt. # TODO Tee does not work on Travis with .NET Core.
 cd $FolderName
 if [ -s Error.txt ] # If Error.txt not empty
 then
