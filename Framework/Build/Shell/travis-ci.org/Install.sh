ec23867c2ac7f40f23f631c4ac08e5f5ff30b6b0
WorkplaceX <info@workplacex.com>
2017-04-17 12:46:47 +0200

    New scripts

--- Framework/Build/Shell/travis-ci.org/Install.sh
+++ Framework/Build/Shell/travis-ci.org/Install.sh
@@ -1,4 +1,6 @@
-set -x
+FolderName=$(pwd) # Working directory
+BASH_XTRACEFD=1 # Print execute command to stdout. Not to stderr.
+set -x # Enable print execute cammands to stdout.
 
 # https://www.microsoft.com/net/core#linuxubuntu
 sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
@@ -6,17 +8,31 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 sudo apt-get update
 sudo apt-get install dotnet-dev-1.0.1
 
-dotnet --version
+function Main
+{
+    dotnet --version
 
-# npm, node version check
-npm --version
-node --version
+    # npm, node version check
+    npm --version
+    node --version
 
-#npm update
-npm install npm@latest -g
+    #npm update
+    npm install npm@latest --loglevel error
 
-# npm, node version check
-npm --version
-node --version
+    # npm, node version check
+    npm --version
+    node --version
 
-npm install gulp
+    npm install gulp --loglevel error
+}
+
+cd $FolderName
+Main 2> >(tee Error.txt) # stderr to stdout and Error.txt.
+cd $FolderName
+if [ -s Error.txt ] # If Error.txt not empty
+then
+    set +x # Disable print command to avoid Error.txt double in log.
+	echo "### Error"
+	echo "$(<Error.txt)" # Print file Error.txt 
+	exit 1 # Set exit code
+fi
