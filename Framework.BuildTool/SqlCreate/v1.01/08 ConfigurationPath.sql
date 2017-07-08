b76ab9fab595699d6608548ca2943fb4d8310433
WorkplaceX <info@workplacex.com>
2017-07-08 16:10:35 +0200

    Load data at the end

---
+++ Framework.BuildTool/SqlCreate/v1.01/08 ConfigurationPath.sql
@@ -0,0 +1,79 @@
+﻿
+CREATE PROCEDURE FrameworkConfigurationPathUpdate
+AS
+
+/* Every Application and Language gets it's Configuration layer */
+INSERT INTO FrameworkConfiguration (ApplicationId)
+SELECT Id FROM FrameworkApplication
+EXCEPT SELECT ApplicationId FROM FrameworkConfiguration
+
+INSERT INTO FrameworkConfiguration (LanguageId)
+SELECT Id FROM FrameworkLanguage
+EXCEPT SELECT LanguageId FROM FrameworkConfiguration
+
+/* Session for every language exclusive */
+INSERT INTO FrameworkSession (Name, LanguageId)
+SELECT NEWID(), Id FROM FrameworkLanguage Language
+WHERE EXISTS 
+(
+	SELECT NULL AS ApplicationId, Language.Id, NULL AS UserId
+	EXCEPT
+	SELECT Session.ApplicationId, Session.LanguageId, Session.UserId FROM FrameworkSession Session
+)
+
+/* Session for every application exclusive */
+INSERT INTO FrameworkSession (Name, ApplicationId)
+SELECT NEWID(), Id FROM FrameworkApplication Application
+WHERE EXISTS 
+(
+	SELECT Application.Id, NULL AS LanguageId, NULL AS UserId
+	EXCEPT
+	SELECT Session.ApplicationId, Session.LanguageId, Session.UserId FROM FrameworkSession Session
+)
+
+DELETE FrameworkConfigurationPath
+
+INSERT INTO FrameworkConfigurationPath (ApplicationId, LanguageId, UserId, SessionId, ConfigurationId, ConfigurationIdContain, Level)
+/* Application layer */
+SELECT
+	Session.ApplicationId, Session.LanguageId, 
+	Session.UserId,
+	Session.Id AS SessionId,
+	Application.ConfigurationId,
+	Application.ConfigurationIdContain,
+	Application.Level
+FROM
+	FrameworkSession Session
+LEFT JOIN
+	FrameworkApplicationHierarchy Application ON Application.ApplicationId = Session.ApplicationId
+WHERE
+	Session.ApplicationId IS NOT NULL
+UNION ALL
+/* Language layer */
+SELECT
+	Session.ApplicationId, Session.LanguageId, 
+	Session.UserId,
+	Session.Id,
+	Language.ConfigurationId,
+	Language.ConfigurationIdContain,
+	Language.Level + 1000
+FROM
+	FrameworkSession Session
+JOIN
+	FrameworkLanguageHierarchy Language ON Language.LanguageId = Session.LanguageId
+UNION ALL
+/* User layer */
+SELECT
+	Session.ApplicationId, Session.LanguageId, 
+	Session.UserId,
+	Session.Id,
+	Session.ConfigurationId,
+	Session.ConfigurationId AS ConfigurationIdContain,
+	2000 AS Level
+FROM
+	FrameworkSession Session
+WHERE
+	Session.ConfigurationId IS NOT NULL
+
+GO
+
--- Framework.BuildTool/SqlCreate/v1.01/09 Data.sql
+++ Framework.BuildTool/SqlCreate/v1.01/09 Data.sql
@@ -1,5 +1,6 @@
 ﻿IF (NOT ISNULL((SELECT Version FROM FrameworkVersion), '') = 'v1.01') RETURN  -- Version Check
 
+/* ApplicationType */
 INSERT INTO FrameworkApplicationType (Name)
 SELECT 'Framework' AS Name
 UNION ALL
@@ -7,31 +8,23 @@ SELECT 'PTC' AS Name
 UNION ALL
 SELECT 'LPN' AS Name
 
+/* Application */
 INSERT INTO FrameworkApplication (Name, ApplicationTypeId, Domain)
 SELECT 'Framework' AS Name, (SELECT Id FROM FrameworkApplicationType WHERE Name = 'Framework') AS ApplicationTypeId, 'framework' AS Domain
-INSERT INTO FrameworkConfiguration (ApplicationId)
-SELECT (SELECT Id From FrameworkApplication WHERE Name = 'Framework')
---
 INSERT INTO FrameworkApplication (Name, ApplicationTypeId, Domain)
 SELECT 'PTC' AS Name, (SELECT Id FROM FrameworkApplicationType WHERE Name = 'PTC'), 'ptc' AS Domain
-INSERT INTO FrameworkConfiguration (ApplicationId)
-SELECT (SELECT Id From FrameworkApplication WHERE Name = 'PTC')
---
 INSERT INTO FrameworkApplication (ParentId, Name, ApplicationTypeId, Domain)
 SELECT (SELECT Id From FrameworkApplication WHERE Name = 'PTC'), 'PTC CH' AS Name, (SELECT Id FROM FrameworkApplicationType WHERE Name = 'PTC'), 'ptcch'
-INSERT INTO FrameworkConfiguration (ApplicationId)
-SELECT (SELECT Id From FrameworkApplication WHERE Name = 'PTC CH')
---
 INSERT INTO FrameworkApplication (ParentId, Name, ApplicationTypeId, Domain)
 SELECT (SELECT Id From FrameworkApplication WHERE Name = 'PTC CH'), 'PTC D' AS Name, (SELECT Id FROM FrameworkApplicationType WHERE Name = 'PTC'), 'ptcd'
-INSERT INTO FrameworkConfiguration (ApplicationId)
-SELECT (SELECT Id From FrameworkApplication WHERE Name = 'PTC D')
---
 INSERT INTO FrameworkApplication (Name, ApplicationTypeId, Domain)
 SELECT 'LPN' AS Name, (SELECT Id FROM FrameworkApplicationType WHERE Name = 'LPN'), 'lpn'
-INSERT INTO FrameworkConfiguration (ApplicationId)
-SELECT (SELECT Id From FrameworkApplication WHERE Name = 'LPN')
 
+GO
+EXEC FrameworkConfigurationPathUpdate
+GO
+
+/* FileStorage */
 INSERT INTO FrameworkFileStorage (ConfigurationId, Name, FileNameUpload)
 SELECT (SELECT ConfigurationId FROM FrameworkConfigurationView WHERE ApplicationName = 'PTC'), 'Outline A', 'OutlineA.docx'
 UNION ALL
@@ -41,21 +34,19 @@ SELECT (SELECT ConfigurationId FROM FrameworkConfigurationView WHERE Application
 UNION ALL
 SELECT (SELECT ConfigurationId FROM FrameworkConfigurationView WHERE ApplicationName = 'PTC CH'), 'Outline B', 'OutlineB2.docx'
 
+/* Language */
 INSERT FrameworkLanguage (ConfigurationId, Name)
 SELECT (SELECT ConfigurationId FROM FrameworkConfigurationView WHERE ApplicationName = 'PTC'), 'Default'
---
 INSERT FrameworkLanguage (ConfigurationId, ParentId, Name)
 SELECT (SELECT ConfigurationId FROM FrameworkConfigurationView WHERE ApplicationName = 'PTC'), (SELECT Id FROM FrameworkLanguage WHERE ConfigurationId = (SELECT ConfigurationId FROM FrameworkConfigurationView WHERE ApplicationName = 'PTC') AND Name = 'Default'), 'English'
---
 INSERT FrameworkLanguage (ConfigurationId, ParentId, Name)
 SELECT (SELECT ConfigurationId FROM FrameworkConfigurationView WHERE ApplicationName = 'PTC'), (SELECT Id FROM FrameworkLanguage WHERE ConfigurationId = (SELECT ConfigurationId FROM FrameworkConfigurationView WHERE ApplicationName = 'PTC') AND Name = 'English'), 'German'
---
 INSERT FrameworkLanguage (ConfigurationId, ParentId, Name)
 SELECT (SELECT ConfigurationId FROM FrameworkConfigurationView WHERE ApplicationName = 'PTC CH'), (SELECT Id FROM FrameworkLanguage WHERE ConfigurationId = (SELECT ConfigurationId FROM FrameworkConfigurationView WHERE ApplicationName = 'PTC') AND Name = 'English'), 'Italian'
---
 INSERT FrameworkLanguage (ConfigurationId, Name)
 SELECT (SELECT ConfigurationId FROM FrameworkConfigurationView WHERE ApplicationName = 'LPN'), 'French'
 
+/* User */
 INSERT INTO FrameworkUser (ApplicationId, Name, LanguageId)
 SELECT (SELECT Id AS ApplicationId FROM FrameworkApplication WHERE Name = 'Framework') AS ApplicationId,  'Admin' AS Name, NULL
 UNION ALL
@@ -63,6 +54,7 @@ SELECT (SELECT Id AS ApplicationId FROM FrameworkApplication WHERE Name = 'LPN')
 UNION ALL
 SELECT (SELECT Id AS ApplicationId FROM FrameworkApplication WHERE Name = 'PTC') AS ApplicationId, 'Hnc' AS Name, NULL
 
+/* Role */
 INSERT INTO FrameworkRole (ApplicationTypeId, Name)
 SELECT (SELECT Id FROM FrameworkApplication WHERE Name = 'Framework'), 'Admin'
 UNION ALL
@@ -70,7 +62,37 @@ SELECT (SELECT Id FROM FrameworkApplication WHERE Name = 'PTC'), 'Coordy'
 UNION ALL
 SELECT (SELECT Id FROM FrameworkApplication WHERE Name = 'PTC'), 'Spky'
 
-INSERT INTO FrameworkSession (Name, ApplicationId, ApplicationTypeId)
-SELECT NEWID(), (SELECT Id FROM FrameworkApplication WHERE Name = 'PTC CH'), (SELECT ApplicationTypeId FROM FrameworkApplication WHERE Name = 'PTC CH') /* User not logged in, no language selected */
-INSERT INTO FrameworkSession (Name, ApplicationId, ApplicationTypeId, UserId)
-SELECT NEWID(), (SELECT Id FROM FrameworkApplication WHERE Name = 'PTC'), (SELECT ApplicationTypeId FROM FrameworkApplication WHERE Name = 'PTC CH'), (SELECT Id FROM FrameworkUser WHERE ApplicationId = (SELECT Id FROM FrameworkApplication WHERE Name = 'PTC CH') AND Name = 'hnc')
+GO
+EXEC FrameworkConfigurationPathUpdate
+GO
+
+/* Add User */
+INSERT INTO FrameworkUser (ApplicationId, Name, LanguageId)
+SELECT (SELECT Id AS ApplicationId FROM FrameworkApplication WHERE Name = 'PTC CH') AS ApplicationId, 'Hnc2' AS Name, (SELECT TOP 1 LanguageLanguageId FROM FrameworkLanguageView WHERE ApplicationName = 'PTC CH' AND Name = 'Italian')
+INSERT INTO FrameworkSession (Name, ApplicationId, ApplicationTypeId, UserId, LanguageId)
+SELECT NEWID(), (SELECT Id FROM FrameworkApplication WHERE Name = 'PTC CH'), (SELECT ApplicationTypeId FROM FrameworkApplication WHERE Name = 'PTC CH'), (SELECT Id FROM FrameworkUser WHERE ApplicationId = (SELECT Id FROM FrameworkApplication WHERE Name = 'PTC CH') AND Name = 'hnc2'), (SELECT LanguageId FROM FrameworkUser WHERE ApplicationId = (SELECT Id FROM FrameworkApplication WHERE Name = 'PTC CH') AND Name = 'hnc2')
+INSERT INTO FrameworkConfiguration (UserId)
+SELECT UserId FROM FrameworkSession
+EXCEPT SELECT UserId FROM FrameworkConfiguration
+UPDATE Session
+SET Session.ConfigurationId = (SELECT Id FROM FrameworkConfiguration WHERE UserId = Session.UserId)
+FROM FrameworkSession Session
+WHERE Session.UserId IS NOT NULL AND Session.ConfigurationId IS NULL
+
+GO
+EXEC FrameworkConfigurationPathUpdate
+GO
+
+/* Add Text */
+INSERT INTO FrameworkText (ConfigurationId, Name)
+SELECT (SELECT TOP 1 ConfigurationIdLanguage FROM FrameworkLanguageView WHERE ApplicationName = 'LPN' AND Name = 'French'), 'Connecter' 
+UNION ALL
+SELECT (SELECT TOP 1 ConfigurationIdLanguage FROM FrameworkLanguageView WHERE ApplicationName = 'PTC' AND Name = 'Default'), 'Login' 
+UNION ALL
+SELECT (SELECT TOP 1 ConfigurationIdLanguage FROM FrameworkLanguageView WHERE ApplicationName = 'PTC' AND Name = 'German'), 'Anmelden' 
+
+GO
+
+EXEC FrameworkConfigurationPathUpdate
+
+GO
