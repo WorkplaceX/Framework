941177e7bee8b8e0ca33dc1c0768d0144fb61283
WorkplaceX <info@workplacex.com>
2018-02-07 22:31:47 +0100

    Update to latest AspNetCore

--- Framework/UtilFramework.cs
+++ Framework/UtilFramework.cs
@@ -39,7 +39,7 @@ namespace Framework
                 // .NET Core 2.0
                 // node 8.9.2 LTS
                 // npm 5.5.1
-                return "v1.073 Server";
+                return "v1.074 Server";
             }
         }
 
