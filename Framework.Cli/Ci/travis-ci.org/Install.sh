29de0dcac87208e472419204283de26ddf182833
WorkplaceX <info@workplacex.com>
2018-07-23 20:30:14 +0200

    Ci

--- Framework.Cli/Ci/travis-ci.org/Install.sh
+++ Framework.Cli/Ci/travis-ci.org/Install.sh
@@ -2,6 +2,7 @@
 set -x # Enable print execute cammands to stdout.
 FolderName=$(pwd) # Working directory
 
+# Install .NET Core 2.1 https://www.microsoft.com/net/download/linux-package-manager/ubuntu14-04/sdk-current
 wget -q https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 
