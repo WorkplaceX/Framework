7baf229dea6c65d90a272bdc2aee42369ee5e4e9
WorkplaceX <info@workplacex.com>
2018-07-23 20:37:10 +0200

    Ci

--- Framework.Cli/Ci/travis-ci.org/Build.sh
+++ Framework.Cli/Ci/travis-ci.org/Build.sh
@@ -1,5 +1,8 @@
-﻿
+﻿#!/bin/bash
+# See also: https://unix.stackexchange.com/questions/27054/bin-bash-no-such-file-or-directory
+
 set -x # Enable print execute cammands to stdout.
+
 echo "Hello Build"
 
 function Main
--- Framework.Cli/Ci/travis-ci.org/Install.sh
+++ Framework.Cli/Ci/travis-ci.org/Install.sh
@@ -1,4 +1,6 @@
 ﻿#!/bin/bash
+# See also: https://unix.stackexchange.com/questions/27054/bin-bash-no-such-file-or-directory
+
 set -x # Enable print execute cammands to stdout.
 FolderName=$(pwd) # Working directory
 
