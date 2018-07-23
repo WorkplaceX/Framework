447f01565db1edd541ff589365b24f95d27135ce
WorkplaceX <info@workplacex.com>
2018-07-23 20:23:15 +0200

    Ci

--- Framework.Cli/Ci/travis-ci.org/Install.sh
+++ Framework.Cli/Ci/travis-ci.org/Install.sh
@@ -1,6 +1,16 @@
-﻿FolderName=$(pwd) # Working directory
+﻿#!/bin/bash
 set -x # Enable print execute cammands to stdout.
+FolderName=$(pwd) # Working directory
+
+wget -q https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.1
+
 echo "Hello"
+echo $FolderName
 
 function Main
 {
