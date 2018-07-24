edd7f62ab26f39e348a7616f9317a9719a313818
WorkplaceX <info@workplacex.com>
2018-07-24 07:52:48 +0200

    Cli

--- Framework.Cli/UtilCli.cs
+++ Framework.Cli/UtilCli.cs
@@ -1,4 +1,32 @@
 ﻿namespace Framework.Cli
 {
+    using Microsoft.Extensions.CommandLineUtils;
+    using System;
 
+    public static class UtilCli
+    {
+        public static void Run(string[] args)
+        {
+            CommandLineApplication commandLineApplication = new CommandLineApplication();
+
+            // Command Build
+            commandLineApplication.Command("Build", (command) => {
+                command.Description = "Build Application";
+                CommandOption commandOption = command.Option("-a", "Build server and client.", CommandOptionType.NoValue);
+                // Execute
+                command.OnExecute(() => {
+                    Console.WriteLine("Build...");
+                    if (commandOption.Value() == "on")
+                    {
+                        Console.WriteLine("All");
+                    }
+                    return 0;
+                });
+                command.HelpOption("-h | --help");
+            });
+
+            commandLineApplication.HelpOption("-h | --help");
+            commandLineApplication.Execute(args);
+        }
+    }
 }
