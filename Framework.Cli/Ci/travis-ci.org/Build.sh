a6a3088cb27b730b10b5a8c8eca909aa58f87410
WorkplaceX <info@workplacex.com>
2018-07-26 11:29:48 +0200

    Cli

--- Framework.Cli/Ci/travis-ci.org/Build.sh
+++ Framework.Cli/Ci/travis-ci.org/Build.sh
@@ -1,21 +1,39 @@
 ﻿#!/bin/bash
 # See also: https://unix.stackexchange.com/questions/27054/bin-bash-no-such-file-or-directory
 
-set -x # Enable print execute cammands to stdout.
+function Main
+{
+	# Cli build
+	cd $FolderName
+	cd Application.Cli
+	dotnet build
 
+	# Config
+    set +x # Prevent AzureGitUrl in log
+    dotnet run --no-build -- config azureGitUrl="$AzureGitUrl" # Set AzureGitUrl
+    set -x
+
+	# Build
+	cd $FolderName
+	cd Application.Cli
+	dotnet run --no-build -- build
+
+	# Deploy
+	cd $FolderName
+	cd Application.Cli
+	dotnet run --no-build -- deploy
+}
+
+set -x # Enable print execute cammands to stdout.
 FolderName=$(pwd) # Working directory
 
-# Cli build
 cd $FolderName
-cd Application.Cli
-dotnet build
-
-# Build
+Main 2> >(tee Error.txt) # Run main with stderr to stdout and Error.txt.
 cd $FolderName
-cd Application.Cli
-dotnet run --no-build -- build
-
-# Deploy
-cd $FolderName
-cd Application.Cli
-dotnet run --no-build -- deploy
+if [ -s Error.txt ] # If Error.txt not empty
+then
+    set +x # Disable print command to avoid Error.txt double in log.
+	echo "### Error"
+	echo "$(<Error.txt)" # Print file Error.txt 
+	exit 1 # Set exit code
+fi
--- Framework.Cli/Ci/travis-ci.org/Install.sh
+++ Framework.Cli/Ci/travis-ci.org/Install.sh
@@ -1,9 +1,6 @@
 ﻿#!/bin/bash
 # See also: https://unix.stackexchange.com/questions/27054/bin-bash-no-such-file-or-directory
 
-set -x # Enable print execute cammands to stdout.
-FolderName=$(pwd) # Working directory
-
 # Install .NET Core 2.1 https://www.microsoft.com/net/download/linux-package-manager/ubuntu14-04/sdk-current
 wget -q https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
@@ -12,15 +9,7 @@ sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install dotnet-sdk-2.1
 
-echo "Hello"
-echo $FolderName
-
-function Main
-{
-    # dotnet, npm, node version check
-	dotnet --version
-    npm --version
-    node --version
-}
-
-Main
+# dotnet, npm, node version check
+dotnet --version
+npm --version
+node --version
