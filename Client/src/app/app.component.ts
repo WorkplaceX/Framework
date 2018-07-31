3975658637955992d58e8b1a2b2d4aac9b1c2487
WorkplaceX <info@workplacex.com>
2018-07-31 11:31:56 +0200

    Client scss

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { DataService } from '../data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   constructor(public DataService: DataService){
--- Framework/Readme.txt
+++ Framework/Readme.txt
@@ -1,4 +1,6 @@
 ﻿ToDo
+-Universal
+-Scss
 -Build badge for visualstudio.com https://stackoverflow.com/questions/48785274/adding-vsts-build-status-to-github-page; https://docs.microsoft.com/en-us/vsts/release-notes/2018/mar-05-vsts#share-deployment-status-using-a-badge
 
 Submodule
