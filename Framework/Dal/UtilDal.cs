d08b644907f43770ed4c4eb0770b8e3aa6cdb5f1
WorkplaceX <info@workplacex.com>
2018-08-16 16:35:18 +0200

    EF Core 2.1

--- Framework/Dal/UtilDal.cs
+++ Framework/Dal/UtilDal.cs
@@ -4,6 +4,10 @@
     using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Metadata;
     using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+    using Microsoft.EntityFrameworkCore.Metadata.Internal;
+    using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+    using Microsoft.EntityFrameworkCore.Storage;
+    using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -23,58 +27,44 @@
         /// </summary>
         private static IMutableModel DbContextModel(Type typeRow)
         {
-            var conventionSet = new ConventionSet();
+            // EF Core 2.1
+            var typeMappingSource = new SqlServerTypeMappingSource(new TypeMappingSourceDependencies(new ValueConverterSelector(new ValueConverterSelectorDependencies())), new RelationalTypeMappingSourceDependencies());
+
+            var conventionSet = SqlServerConventionSetBuilder.Build();
             var builder = new ModelBuilder(conventionSet);
+
             // Build model
             var entity = builder.Entity(typeRow);
             SqlTableAttribute tableAttribute = (SqlTableAttribute)typeRow.GetTypeInfo().GetCustomAttribute(typeof(SqlTableAttribute));
             entity.ToTable(tableAttribute.SqlTableName, tableAttribute.SqlSchemaName); // By default EF maps sql table name to class name.
-            bool isPrimaryKey = false;
-            PropertyInfo[] propertyInfoList = UtilDal.TypeRowToPropertyList(typeRow);
+            PropertyInfo[] propertyInfoList = TypeRowToPropertyList(typeRow);
+            bool isPrimaryKey = false; // Sql view 
             foreach (PropertyInfo propertyInfo in propertyInfoList)
             {
                 SqlFieldAttribute columnAttribute = (SqlFieldAttribute)propertyInfo.GetCustomAttribute(typeof(SqlFieldAttribute));
-                if (columnAttribute == null || columnAttribute.SqlFieldName == null) // Calculated column. Do not include it in sql select. For example button added to row.
+                if (columnAttribute == null || columnAttribute.SqlFieldName == null) // Calculated column. Do not include it in sql select.
                 {
-                    if (columnAttribute != null && columnAttribute.IsPrimaryKey)
-                    {
-                        throw new Exception("Primary key can not be calculated!");
-                    }
                     entity.Ignore(propertyInfo.Name);
                 }
                 else
                 {
-                    // Primary key
-                    if (columnAttribute != null && columnAttribute.IsPrimaryKey)
+                    if (columnAttribute.IsPrimaryKey)
                     {
                         isPrimaryKey = true;
-                        entity.HasKey(propertyInfo.Name);
-                        entity.Property(propertyInfo.Name).ValueGeneratedOnAdd(); // Read back auto increment key value.
+                        entity.HasKey(propertyInfo.Name); // Prevent null exception if primary key name is not "Id".
                     }
                     entity.Property(propertyInfo.PropertyType, propertyInfo.Name).HasColumnName(columnAttribute.SqlFieldName);
+                    CoreTypeMapping coreTypeMapping = typeMappingSource.FindMapping(propertyInfo.PropertyType);
+                    UtilFramework.Assert(coreTypeMapping != null);
+                    entity.Property(propertyInfo.PropertyType, propertyInfo.Name).HasAnnotation(CoreAnnotationNames.TypeMapping, coreTypeMapping);
                 }
             }
+
             if (isPrimaryKey == false)
             {
-                // No primary key defined. For example View. In order to prevent NullException when inserting row,
-                // set artificial "Primary Key" on first column (not calculated column). See also method UtilDal.Insert();
-                PropertyInfo propertyInfoFirst = null;
-                foreach (PropertyInfo propertyInfo in propertyInfoList)
-                {
-                    SqlFieldAttribute columnAttribute = (SqlFieldAttribute)propertyInfo.GetCustomAttribute(typeof(SqlFieldAttribute));
-                    if (columnAttribute != null && columnAttribute.SqlFieldName != null)
-                    {
-                        propertyInfoFirst = propertyInfo;
-                        break;
-                    }
-                }
-                if (propertyInfoFirst == null)
-                {
-                    propertyInfoFirst = propertyInfoList.First(); // No artificial primary key found! For example calculated rows (views) which read only and do not write back.
-                }
-                entity.HasKey(propertyInfoFirst.Name);
-                entity.Property(propertyInfoFirst.Name).ValueGeneratedOnAdd(); // Read back auto increment key value.
+                entity.HasKey(propertyInfoList.First().Name); // Prevent null exception if name of first field (of view) is not "Id".
             }
+
             var model = builder.Model;
             return model;
         }
@@ -90,17 +80,16 @@
             {
                 throw new Exception("ConnectionString is null! (See also file: ConfigFramework.json)");
             }
-            options.UseSqlServer(connectionString); // See also: ConnectionManagerServer.json (Data Source=localhost; Initial Catalog=Database; Integrated Security=True;)
+            options.UseSqlServer(connectionString); // See also: ConfigFramework.json // (Data Source=localhost; Initial Catalog=Application; Integrated Security=True;)
             options.UseModel(DbContextModel(typeRow));
             DbContext result = new DbContext(options.Options);
-            //
+
             return result;
         }
 
         public static IQueryable Query(Type typeRow)
         {
             DbContext dbContext = DbContext(typeRow);
-            dbContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking; // For SQL views. No primary key.
             IQueryable query = (IQueryable)(dbContext.GetType().GetTypeInfo().GetMethod("Set").MakeGenericMethod(typeRow).Invoke(dbContext, null));
             return query;
         }
