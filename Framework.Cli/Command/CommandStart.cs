a9aed45359d27e3dbc74891b1617881058f2a083
WorkplaceX <info@workplacex.com>
2018-12-21 22:58:32 +0100

    CommandStart update

--- Framework.Cli/Command/CommandStart.cs
+++ Framework.Cli/Command/CommandStart.cs
@@ -16,7 +16,7 @@
             string folderName = UtilFramework.FolderName + @"Application.Server/";
             UtilCli.DotNet(folderName, "build");
             UtilCli.DotNet(folderName, "run --no-build", false);
-            UtilCli.OpenWebBrowser("http://localhost:56093/"); // For port setting see also: Application.Server\Properties\launchSettings.json
+            UtilCli.OpenWebBrowser("https://localhost:56094/"); // For port setting see also: Application.Server\Properties\launchSettings.json
         }
     }
 }
