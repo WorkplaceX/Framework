241a3a8752b08958d5e2a2651228ba0f41c48fd6
WorkplaceX <info@workplacex.com>
2019-10-29 22:18:34 +0100

    Update build version check

--- Framework.Cli/Ci/travis-ci.org/Build.sh
+++ Framework.Cli/Ci/travis-ci.org/Build.sh
@@ -9,7 +9,7 @@ FileNameErrorText=$FolderName"Error.txt"
 BASH_XTRACEFD=1 # Print execute command to stdout. Not to stderr.
 set -x # Enable print execute cammands to stdout.
 
-# ubuntu, dotnet, npm, node version check
+# ubuntu, dotnet, node, npm, version check
 lsb_release -a
 dotnet --version
 node --version
