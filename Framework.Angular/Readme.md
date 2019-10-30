79b12feff1bd66852f9c35044545d4f0b05a050d
WorkplaceX <info@workplacex.com>
2019-10-30 23:09:35 +0100

    Add folder Framework.Angular

---
+++ Framework.Angular/Readme.md
@@ -0,0 +1,15 @@
+# Angular 8 Client Application
+
+## Version Check
+```
+node --version # v12.13.0
+npm --version # 6.12.0
+ng --version # Angular CLI: 8.3.15
+npm list -g --depth 0 # List globally installed packages
+```
+
+## Install
+```
+ng new application
+ng add @nguniversal/express-engine --clientProject application
+```
--- Framework.Cli/Ci/travis-ci.org/Build.sh
+++ Framework.Cli/Ci/travis-ci.org/Build.sh
@@ -9,11 +9,14 @@ FileNameErrorText=$FolderName"Error.txt"
 BASH_XTRACEFD=1 # Print execute command to stdout. Not to stderr.
 set -x # Enable print execute cammands to stdout.
 
-# ubuntu, dotnet, node, npm version check
+# Ubuntu, dotnet, node, npm version check
 lsb_release -a
 dotnet --version
 node --version
 npm --version
+
+# List globally installed packages
+npm list -g --depth 0
 
 function Main
 {
