57e8c0df67e3ac713b258c4d910c9f03b3d3e393
WorkplaceX <info@workplacex.com>
2019-11-18 16:38:08 +0100

    Update for Ubuntu 18.04

--- Framework.Cli/Ci/travis-ci.org/Install.sh
+++ Framework.Cli/Ci/travis-ci.org/Install.sh
@@ -1,10 +1,11 @@
 ﻿#!/bin/bash
 # See also: https://unix.stackexchange.com/questions/27054/bin-bash-no-such-file-or-directory
 
-# Install .NET Core 3.0 https://dotnet.microsoft.com/download/linux-package-manager/ubuntu16-04/sdk-current
-wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+# Install .NET Core 3.0 https://dotnet.microsoft.com/download/linux-package-manager/ubuntu18-04/sdk-current
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 
+sudo add-apt-repository universe
 sudo apt-get update
 sudo apt-get install apt-transport-https
 sudo apt-get update
