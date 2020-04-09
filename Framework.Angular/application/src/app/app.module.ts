7a43a61ace8ae7e9a6531c87fcc80ba2bd4e79ee
WorkplaceX <info@workplacex.com>
2020-04-09 09:19:26 +0200

    ng generate component framework --skip-import

--- Framework.Angular/application/src/app/app.module.ts
+++ Framework.Angular/application/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FrameworkComponent } from './framework/framework.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, FrameworkComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' })
---
+++ Framework.Angular/application/src/app/framework/framework.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-framework',
+  templateUrl: './framework.component.html',
+  styleUrls: ['./framework.component.scss']
+})
+export class FrameworkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
