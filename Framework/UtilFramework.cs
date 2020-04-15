f4a60269a72fa285bb4785b1a2621fe5de2f62b2
WorkplaceX <info@workplacex.com>
2020-04-15 18:46:50 +0200

    Data QueryExecute

--- Framework/UtilFramework.cs
+++ Framework/UtilFramework.cs
@@ -38,7 +38,7 @@ namespace Framework
                 // npm run ng -- --version (Framework/Framework.Angular/application/)
                 // Angular CLI: 8.3.15
 
-                return "v3.30.11";
+                return "v3.30.12";
             }
         }
 
