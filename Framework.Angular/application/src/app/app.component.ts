7b1e804c13f86b5c0f3203e426ac38a36f45856a
WorkplaceX <info@workplacex.com>
2020-08-24 20:53:55 +0200

    Access local server

--- Framework.Angular/application/src/app/app.component.ts
+++ Framework.Angular/application/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DataService, CommandJson } from './data.service';
-import {Location, LocationStrategy, PathLocationStrategy, PopStateEvent} from '@angular/common';
+import { Location, LocationStrategy, PathLocationStrategy, PopStateEvent} from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,9 @@ import {Location, LocationStrategy, PathLocationStrategy, PopStateEvent} from '@
 })
 export class AppComponent {
   title = 'application';
+  localServer = 'localhost:44308/';
 
-  constructor(public dataService: DataService, private location: Location) {
+  constructor(public dataService: DataService, private location: Location, private http: HttpClient) {
     this.location.onUrlChange((url: string, state: unknown) => {
       var path: string = <string>state; // See also file data.service.ts method call location.go();
       if (path == null) {
@@ -21,6 +23,11 @@ export class AppComponent {
       // LinkPost for backward and forward navigation.
       this.dataService.update(<CommandJson> { Command: 17, ComponentId: this.dataService.json.Id, NavigateLinkPath: path});
     });
+  }
+  
+  myClick() {
+    this.localServer = "Get...";
+    this.http.get("https://localhost:44308/", {responseType: 'text'}).subscribe((data) => this.localServer = data);
   }
 
   trackBy(index: any, item: any) {
