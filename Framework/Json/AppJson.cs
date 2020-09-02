c0802f679e46048571a89b877f269f56896cdbb9
WorkplaceX <info@workplacex.com>
2020-09-02 13:40:30 +0200

    Update comment

--- Framework/Json/AppJson.cs
+++ Framework/Json/AppJson.cs
@@ -724,7 +724,7 @@
         public class NavigateResult
         {
             /// <summary>
-            /// Gets or sets IsSession. If true, method NavigateSessionAsync(); with session data available is called next.
+            /// Gets or sets IsSession. If true, session is requested and method NavigateSessionAsync(); with session data available is called next.
             /// </summary>
             public bool IsSession { get; set; }
 
