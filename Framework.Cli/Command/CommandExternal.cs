121c86deb2fde6004c68e42f5682e3ad4590caa6
WorkplaceX <info@workplacex.com>
2020-09-27 20:57:12 +0200

    Update cli command external add option delete

--- Framework.Cli/Command/CommandExternal.cs
+++ Framework.Cli/Command/CommandExternal.cs
@@ -1,5 +1,6 @@
 ﻿namespace Framework.Cli.Command
 {
+    using Microsoft.Extensions.CommandLineUtils;
     using System;
 
     /// <summary>
@@ -8,27 +9,70 @@
     internal class CommandExternal : CommandBase
     {
         public CommandExternal(AppCli appCli)
-            : base(appCli, "external", "Run external prebuild .NET script.")
+            : base(appCli, "external", "Copy ExternalGit folders.")
         {
 
         }
 
-        protected internal override void Execute()
+        private CommandOption optionDelete;
+
+        protected internal override void Register(CommandLineApplication configuration)
+        {
+            optionDelete = configuration.Option("--delete", "Delete ExternalGit dest folders.", CommandOptionType.NoValue);
+        }
+
+        private void ExecuteDelete()
+        {
+            var args = UtilExternal.ExternalArgs();
+
+            // Delete folder App/
+            Console.WriteLine("Delete dest folder App/");
+            UtilCli.FolderDelete(args.AppDestFolderName);
+
+            // Copy folder Database/
+            Console.WriteLine("Delete dest folder App/");
+            UtilCli.FolderDelete(args.DatabaseDestFolderName);
+
+            // Copy folder CliApp/
+            Console.WriteLine("Delete dest folder CliApp/");
+            UtilCli.FolderDelete(args.CliAppDestFolderName);
+
+
+            // Copy folder CliDatabase/
+            Console.WriteLine("Delete dest folder CliDatabase/");
+            UtilCli.FolderDelete(args.CliDatabaseDestFolderName);
+
+
+            // Copy folder CliDeployDb/
+            Console.WriteLine("Delete dest folder CliDeployDb/");
+            UtilCli.FolderDelete(args.CliDeployDbDestFolderName);
+
+            // Copy folder Application.Website/
+            Console.WriteLine("Delete dest folder Application.Website/");
+            UtilCli.FolderDelete(args.WebsiteDestFolderName);
+        }
+
+        private void ExecuteCopy()
         {
             var args = UtilExternal.ExternalArgs();
 
             // Copy folder App/
+            Console.WriteLine("Copy folder App/");
             args.FolderCopy(args.AppSourceFolderName, args.AppDestFolderName);
 
             // Copy folder Database/
+            Console.WriteLine("Copy folder Database/");
             args.FolderCopy(args.DatabaseSourceFolderName, args.DatabaseDestFolderName);
 
             // Copy folder CliApp/
             args.FolderCopy(args.CliAppSourceFolderName, args.CliAppDestFolderName);
+            Console.WriteLine("Copy folder CliApp/");
 
             // Copy folder CliDatabase/
             args.FolderCopy(args.CliDatabaseSourceFolderName, args.CliDatabaseDestFolderName);
+            Console.WriteLine("Copy folder CliDatabase/");
 
+            Console.WriteLine("Update file DatabaseIntegrate.cs");
             args.FileReplaceLine(
                 args.CliDatabaseDestFolderName + "DatabaseIntegrate.cs",
                 "    public static class FrameworkConfigGridIntegrateApplicationCli",
@@ -40,6 +84,7 @@
                 "    public static class FrameworkConfigFieldIntegrateApplicationCli" + "External" + args.ExternalProjectName);
 
             // Copy folder CliDeployDb/
+            Console.WriteLine("Copy folder CliDeployDb/");
             args.FolderCopy(args.CliDeployDbSourceFolderName, args.CliDeployDbDestFolderName);
 
             // Copy folder Application.Website/
@@ -47,6 +92,21 @@
             args.FolderCopy(args.WebsiteSourceFolderName, args.WebsiteDestFolderName);
 
             AppCli.CommandExternal(args);
+        }
+
+
+        protected internal override void Execute()
+        {
+            if (optionDelete.OptionGet())
+            {
+                // Delete ExternalGit
+                ExecuteDelete();
+            }
+            else
+            {
+                // Copy ExternalGit
+                ExecuteCopy();
+            }
         }
     }
 }
