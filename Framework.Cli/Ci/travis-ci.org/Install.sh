222bb03228a0546800156c37adad995ad703fb0f
WorkplaceX <info@workplacex.com>
2020-11-15 20:38:07 +0100

    .NET 5.0

--- Framework.Cli/Ci/travis-ci.org/Install.sh
+++ Framework.Cli/Ci/travis-ci.org/Install.sh
@@ -1,15 +1,11 @@
 ﻿#!/bin/bash
 # See also: https://unix.stackexchange.com/questions/27054/bin-bash-no-such-file-or-directory
 
-# Install .NET Core 3.1 https://dotnet.microsoft.com/download/linux-package-manager/ubuntu18-04/sdk-current
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+# Install .NET 5.0 https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu#1804-
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 
-# Troubleshoot the package manager https://docs.microsoft.com/en-us/dotnet/core/install/linux-package-manager-ubuntu-1804#troubleshoot-the-package-manager
-sudo dpkg --purge packages-microsoft-prod && sudo dpkg -i packages-microsoft-prod.deb
-
-sudo add-apt-repository universe
-sudo apt-get update
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install dotnet-sdk-3.1
+sudo apt-get update; \
+sudo apt-get install -y apt-transport-https && \
+sudo apt-get update && \
+sudo apt-get install -y dotnet-sdk-5.0
