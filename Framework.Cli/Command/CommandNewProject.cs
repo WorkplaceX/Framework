c085749aa7815187cdefb77c0dc4da9491a505b5
WorkplaceX <info@workplacex.com>
2021-03-05 22:41:56 +0100

    Update Website

--- Framework.Cli/Command/CommandNewProject.cs
+++ Framework.Cli/Command/CommandNewProject.cs
@@ -1,6 +1,4 @@
-﻿using Framework.Server;
-using System;
-using System.Diagnostics;
+﻿using System;
 using System.IO;
 using System.Runtime.InteropServices;
 
@@ -19,15 +17,8 @@ namespace Framework.Cli.Command
 
         private static void FolderCopy(string folderNameSource, string folderNameDest)
         {
-            // Check dest folder is empty
-            if (Directory.GetFileSystemEntries(folderNameDest).Length > 0)
-            {
-                UtilCli.ConsoleWriteLineColor("This dest folder needs to be empty!", ConsoleColor.Red);
-                return;
-            }
-
             // Copy ApplicationHelloWorld template
-            Console.WriteLine(string.Format("Copy {0} to {1}", folderNameSource, folderNameDest));
+            // Console.WriteLine(string.Format("Copy {0} to {1}", folderNameSource, folderNameDest));
             UtilCli.FolderCopy(folderNameSource, folderNameDest);
         }
 
@@ -41,8 +32,17 @@ namespace Framework.Cli.Command
             string folderNameSource = folderNameApplicationHelloWorld;
             string folderNameDest = Directory.GetCurrentDirectory().Replace(@"\", "/") + "/";
 
-            Console.WriteLine("Source=" + folderNameSource);
-            Console.WriteLine("Dest=" + folderNameDest);
+            // Console.WriteLine("Source=" + folderNameSource);
+            // Console.WriteLine("Dest=" + folderNameDest);
+
+            // Check dest folder is empty
+            if (Directory.GetFileSystemEntries(folderNameDest).Length > 0)
+            {
+                UtilCli.ConsoleWriteLineColor("This folder needs to be empty!", ConsoleColor.Red);
+                return;
+            }
+
+            Console.WriteLine("Installing...");
 
             // Copy ApplicationHelloWorld
             FolderCopy(folderNameSource, folderNameDest);
@@ -52,14 +52,14 @@ namespace Framework.Cli.Command
             FolderCopy(folderNameFramework, folderNameDest + "Framework/");
 
             // Start new cli
-            Console.WriteLine();
+            UtilCli.ConsoleWriteLineColor("Installation successfull!", ConsoleColor.Green);
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                Process.Start("cli.cmd").WaitForExit();
+                UtilCli.ConsoleWriteLineColor("Start cli now with command .\\cli.cmd", ConsoleColor.DarkGreen);
             }
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                Process.Start("cli.sh").WaitForExit();
+                UtilCli.ConsoleWriteLineColor("Start cli now with command ./cli.sh", ConsoleColor.DarkGreen);
             }
         }
     }
