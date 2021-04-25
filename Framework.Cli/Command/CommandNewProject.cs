e911aad7a85955e62e10c6a65b6e65cbb00a18d7
WorkplaceX <info@workplacex.com>
2021-04-25 22:29:41 +0200

    Update command new

--- Framework.Cli/Command/CommandNewProject.cs
+++ Framework.Cli/Command/CommandNewProject.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 
@@ -6,6 +7,7 @@ namespace Framework.Cli.Command
 {
     /// <summary>
     /// Cli command to create a new project from template.
+    /// For debug run in an empty folder: "dotnet run --project C:\Temp\GitHub\ApplicationDemo\ExternalGit\ApplicationDoc\Framework\Framework.Cli -- new".
     /// </summary>
     internal class CommandNewProject : CommandBase
     {
@@ -36,20 +38,37 @@ namespace Framework.Cli.Command
             // Console.WriteLine("Dest=" + folderNameDest);
 
             // Check dest folder is empty
-            if (Directory.GetFileSystemEntries(folderNameDest).Length > 0)
+            var list = Directory.GetFileSystemEntries(folderNameDest);
+            var isGitOnly = list.Length == 1 && list[0] == folderNameDest + ".git"; // Empty git folder
+            if (list.Length > 0 && !isGitOnly)
             {
                 UtilCli.ConsoleWriteLineColor("This folder needs to be empty!", ConsoleColor.Red);
                 return;
             }
+
+            var isSubmodule = UtilCli.ConsoleReadYesNo("For Framework use Submodule?");
 
             Console.WriteLine("Installing...");
 
             // Copy ApplicationHelloWorld
             FolderCopy(folderNameSource, folderNameDest);
 
-            // Copy Framework
-            UtilCli.FolderCreate(folderNameDest + "Framework/");
-            FolderCopy(folderNameFramework, folderNameDest + "Framework/");
+            if (isSubmodule)
+            {
+                var info = new ProcessStartInfo
+                {
+                    WorkingDirectory = folderNameDest,
+                    FileName = "git",
+                    Arguments = "submodule add https://github.com/WorkplaceX/Framework.git"
+                };
+                Process.Start(info).WaitForExit();
+            }
+            else
+            {
+                // Copy Framework
+                UtilCli.FolderCreate(folderNameDest + "Framework/");
+                FolderCopy(folderNameFramework, folderNameDest + "Framework/");
+            }
 
             // Start new cli
             UtilCli.ConsoleWriteLineColor("Installation successfull!", ConsoleColor.Green);
